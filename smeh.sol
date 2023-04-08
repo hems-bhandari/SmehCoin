c3eb81d36d6032e26bd598cf0e563dbdd28a4cf6
Hems <60810039+hems-bhandari@users.noreply.github.com>
2023-04-08 11:10:05 +0545

    Update smart contract

--- smeh.sol
+++ smeh.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-contract SMEHcoin {
+contract SMEHcontract {
     mapping (address => uint256) public balances;
     uint256 public totalSupply;
 
@@ -20,7 +20,3 @@ contract SMEHcoin {
         return balances[_owner];
     }
 }
-
-//  SMEHcoin smart contract defines a mapping that associates addresses with balances, as well as a total supply of coins. The constructor function is called when the contract is deployed, and initializes the total supply of coins and assigns them all to the address that deploys the contract.
-
-// The transfer function allows an address to transfer coins to another address, as long as the sender has a sufficient balance. The balanceOf function allows an address to query its current balance.
