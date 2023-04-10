8959af56985723bb1dd4a15c234e193efdb2da55
Hems Bhandari <hemsbhandari04@gmail.com>
2023-04-10 23:09:53 +0545

    Add more functionalities

--- smeh.sol
+++ smeh.sol
@@ -2,25 +2,58 @@ pragma solidity ^0.8.0;
 
 contract SMEHcoin {
     mapping (address => uint256) public balances;
+    mapping (address => mapping (address => uint256)) allowed;
     uint256 public totalSupply;
+    address public owner;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     constructor(uint256 _totalSupply) {
         totalSupply = _totalSupply;
+        owner = msg.sender;
         balances[msg.sender] = _totalSupply;
     }
 
     function transfer(address _to, uint256 _value) public returns (bool) {
-        require(balances[msg.sender] >= _value, "Insufficient balance.");
+        require(_value <= balances[msg.sender], "Insufficient balance.");
         balances[msg.sender] -= _value;
         balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function balanceOf(address _owner) public view returns (uint256) {
         return balances[_owner];
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_value <= balances[_from], "Insufficient balance.");
+        require(_value <= allowed[_from][msg.sender], "Allowance exceeded.");
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowed[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function mint(uint256 _amount) public {
+        require(msg.sender == owner, "Only the owner can mint new coins.");
+        totalSupply += _amount;
+        balances[msg.sender] += _amount;
+        emit Transfer(address(0), msg.sender, _amount);
+    }
+
+    function burn(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance.");
+        totalSupply -= _amount;
+        balances[msg.sender] -= _amount;
+        emit Transfer(msg.sender, address(0), _amount);
+    }
 }
-
-//  SMEHcoin smart contract defines a mapping that associates addresses with balances, as well as a total supply of coins. The constructor function is called when the contract is deployed, and initializes the total supply of coins and assigns them all to the address that deploys the contract.
-
-// The transfer function allows an address to transfer coins to another address, as long as the sender has a sufficient balance. The balanceOf function allows an address to query its current balance.
